54095bfbf9b52b32b1cf7cadba39f616415f4390
Alexandre ADOLPHE <contact@alexandreadolphe.com>
2018-05-27 23:49:14 +0400

    debut ajout de fonctionnalité jquery (ne fonctionnnant pas encore)

--- js/mains.js
+++ js/mains.js
@@ -3,4 +3,12 @@ $(document).ready(function(){
     $(window).on('load',(function(){
         $('#loader').fadeOut(500);
     }));
+
+    
+    if($("#collapse_portfolio").is(".show")){
+        $("#buttonCollapse").html("Voir Moins");
+    }else{
+        $("#buttonCollapse").html("Voir Plus xxxx");
+    }
+
 });
